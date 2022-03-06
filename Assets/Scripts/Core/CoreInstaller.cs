cd0025398b438df8cdd1eeddd67c4f9181623264
Maleriya <50017431+Maleriya@users.noreply.github.com>
2022-03-07 01:01:05 +0300

    Reactive-MVP Base

---
+++ Assets/Scripts/Core/CoreInstaller.cs
@@ -0,0 +1,12 @@
+﻿using Zenject;
+
+public class CoreInstaller : MonoInstaller
+{
+    public override void InstallBindings()
+    {
+        Container.BindInterfacesAndSelfTo<TimeModel>().AsSingle();
+
+        // если появится еще лишний интерфейс, для которого не нужна привязка, то будет актуально это
+        // Container.Bind(typeof(ITimeModule), typeof(ITickable)).To(typeof(TimeModel)).AsSingle();
+    }
+}
