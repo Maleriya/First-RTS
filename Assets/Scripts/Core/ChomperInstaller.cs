98ef7706334fa3edf6d1b4d65a045e34efaf766d
Maleriya <50017431+Maleriya@users.noreply.github.com>
2022-03-10 15:26:58 +0300

    Practice task - Who is the winner (only solder can win)

--- Assets/Scripts/Core/ChomperInstaller.cs
+++ Assets/Scripts/Core/ChomperInstaller.cs
@@ -5,7 +5,7 @@ public class ChomperInstaller : MonoInstaller
     public override void InstallBindings()
     {
         Container.Bind<IHealthHolder>().FromComponentInChildren();
-        Container.Bind<float>().WithId("AttackDistance").FromInstance(5f);
+        Container.Bind<float>().WithId("AttackDistance").FromInstance(1.5f);
         Container.Bind<int>().WithId("AttackPeriod").FromInstance(1400);
     }
 }
--- Assets/Scripts/Core/CoreInstaller.cs
+++ Assets/Scripts/Core/CoreInstaller.cs
@@ -1,10 +1,14 @@
 ﻿using Zenject;
+using UnityEngine;
 
 public class CoreInstaller : MonoInstaller
 {
+    [SerializeField] private GameStatus _gameStatus;
+
     public override void InstallBindings()
     {
         Container.BindInterfacesAndSelfTo<TimeModel>().AsSingle();
+        Container.Bind<IGameStatus>().FromInstance(_gameStatus);
 
         // если появится еще лишний интерфейс, для которого не нужна привязка, то будет актуально это
         // Container.Bind(typeof(ITimeModule), typeof(ITickable)).To(typeof(TimeModel)).AsSingle();
